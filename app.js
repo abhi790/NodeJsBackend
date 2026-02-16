2adc41cc12956a84ea5a66fc2f4c3c5b319ca9b4
abhi790 <abhimanyu.roy4000@gmail.com>
2026-02-16 10:26:29 +0530

    we have install express dependencies and started a simple server at localhost on a specified port no, we have also made our server capable of receiving a simple get request to it's home route and send a successful text message to the client

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("server is running....");
+const express = require("express");
+// express method will return an app
+const app = express();
+
+// start listening
+app.listen(5000, "localhost", () => {
+  console.log("server is started successfully");
+});
+
+// simply receiving get request to homepage '/', called home route
+app.get("/", (req, res) => {
+  res.send("List of all hotels.....");
+});

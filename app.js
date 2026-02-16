f3007919369439c04b1341a7658349a938cd8501
abhi790 <abhimanyu.roy4000@gmail.com>
2026-02-16 21:44:46 +0530

    we learned how to listen to post request from post by sending body a json object to app.use(express.json()), we have created a app.post method and make .gitignore file again stop tracking data folder.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+const { count } = require("console");
 const express = require("express");
 const fs = require("fs");
 // express method will return an app
 const app = express();
 
+app.use(express.json());
 // read hotels.json file
 const hotels = JSON.parse(fs.readFileSync("./data/hotels.json", "utf-8"));
 // GET: localhost:5000/api/v1/hotels
@@ -10,6 +12,23 @@ app.get("/api/v1/hotels", (req, res) => {
   res.status(200).json({
     status: "success",
     data: { hotels: hotels, count: hotels.length },
+  });
+});
+
+// POST: localhost:3000/api/v1/hotels
+app.post("/api/v1/hotels", (req, res) => {
+  const newId = hotels[hotels.length - 1].id + 1;
+  const newHotel = Object.assign({ id: newId }, req.body);
+  hotels.push(newHotel);
+  // Write
+  fs.writeFile("./data/hotels.json", JSON.stringify(hotels), () => {
+    res.status(201).json({
+      status: "success",
+      data: {
+        hotels: newHotel,
+        count: hotels.length,
+      },
+    });
   });
 });
 

4cb8b1eef3ad81212370e2110c04f4880caeddfa
abhi790 <abhimanyu.roy4000@gmail.com>
2026-02-17 17:23:01 +0530

    we have used a logger called morgan to log, we installed it app.use(morgan()),

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 const express = require("express");
 const hotelRouter = require("./routers/hotelsRouter");
+const morgan = require("morgan");
 
 // CREATING AN EXPRESS APP
 const app = express();
 
-// USING MIDDLEWARE
-app.use(express.json());
 // custom middlewere need to have 3 parameter function
 const logger = (req, res, next) => {
   console.log(`User requested method : ${req.method} and url is ${req.url}`);
   // next() mandatory task - need to call to give other middleware to execute their task
   next();
 };
+
+// USING MIDDLEWARE
+app.use(express.json());
+
+// using morgan middleware - morgan(format,options)
+app.use(morgan("dev")); //run asynchronously so that it get status code and time took to respond , it is attached to finish event of the response object.
+
 // setup middleware to use by the app
-app.use(logger); //.use(logger) - logger will be executed for all the endpoints our app has hotels, users, staffs and so on
-app.use("/api/v1/hotels", logger); // .use(path,logger) - logger will be executed for a particular endpoints(/api/v1/hotels) metioned and not for "/api/v1/users","/api/v1/staffs" etc., and this is true for all middleware
+app.use(logger);
 
 // ADDING ROUTES FOR APP
 // routes for hotels

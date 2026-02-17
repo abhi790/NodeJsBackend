b9d9fd5a86f9b4470e7a5c9e4e4045c720c8b680
abhi790 <abhimanyu.roy4000@gmail.com>
2026-02-17 16:44:57 +0530

    we learned about middleware and understand how to create a middleware, how the order is matter for the middle placement in order to execute it correctly, We used express.json() middleware to attached request payload to the body property of the request object,  the order of middleware is important, the order in which it is defined it will gets executed by the app.

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const app = express();
 
 // USING MIDDLEWARE
 app.use(express.json());
+// custom middlewere need to have 3 parameter function
+const logger = (req, res, next) => {
+  console.log(`User requested method : ${req.method} and url is ${req.url}`);
+  // next() mandatory task - need to call to give other middleware to execute their task
+  next();
+};
+// setup middleware to use by the app
+app.use(logger); //.use(logger) - logger will be executed for all the endpoints our app has hotels, users, staffs and so on
+app.use("/api/v1/hotels", logger); // .use(path,logger) - logger will be executed for a particular endpoints(/api/v1/hotels) metioned and not for "/api/v1/users","/api/v1/staffs" etc., and this is true for all middleware
 
 // ADDING ROUTES FOR APP
 // routes for hotels

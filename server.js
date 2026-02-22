b727426e69bf523c49d7a1eaa085fb8fc778ad8e
abhi790 <abhimanyu.roy4000@gmail.com>
2026-02-22 18:25:09 +0530

    we have sucessfully connected our nodejs backend to the mongodb altas on cloud and for doing this we have installed mongoose an ODM - Object Data Model tools to efficiently work with the schema, connecting with database, create schema, set data validation and many more things. we have also used mongoose.connection property to listen to certain event like disconnected, open, connected

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+const connString =
+  "mongodb+srv://admin:test1234@myhotel-cluster.li5aws8.mongodb.net/bookmystay?appName=myhotel-cluster";
+mongoose
+  .connect(connString)
+  .then((conn) => console.log("Connection to db is successful"))
+  .catch((err) => console.log("Could not connect to db", err.message));
+
+//   get the connection property to listen any event to the connection of database
+// listen for any disconnection event to the database
+mongoose.connection.on("disconnected", () => console.log("disconnected"));
+// listen for open event
+mongoose.connection.on("open", () => console.log("open"));
+// listen for connected event
+mongoose.connection.on("connected", () => console.log("connected"));
 
 // start listening
 app.listen(5000, "localhost", () => {

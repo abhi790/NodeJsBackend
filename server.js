71a3e1055d12398c078b01e9b2ce8fd6ac8ca9e2
abhi790 <abhimanyu.roy4000@gmail.com>
2026-02-22 19:19:26 +0530

    we learned many things in this lecture - 98 Mongoose model. We first created a schema using mongoose.Schema({}) and then created a Hotel Model using mongoose.model(model name, schema name, collections name optional), and also created our first hotel document hotel1 and successfuly inserted that document inside our hotels collections using hotel1.save() -  which is an async function you know what i mean, .then and .catch :-). also we resolve a critical issue which encouter when we tried to use await keyword which using hotel1.save(), that lead to a confusion to converting import and export using require and module.exports to change but I sticked to require as off now, will change later if needed. I have used claude to solve the issue quickly.

--- server.js
+++ server.js
@@ -15,6 +15,21 @@ mongoose.connection.on("open", () => console.log("open"));
 // listen for connected event
 mongoose.connection.on("connected", () => console.log("connected"));
 
+// HOtel schema
+const hotelSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  city: String,
+});
+
+// Create Model mongoose.model(model name, schema, collections optional(automatically plural to model name))
+const Hotel = mongoose.model("Hotel", hotelSchema);
+const hotel1 = new Hotel({ name: "Test 1", price: 1200, city: "Mumbai" });
+hotel1
+  .save()
+  .then((resolve) => console.log("Hotel Saved"))
+  .catch((error) => console.log("Didnot save the hotel", error.message)); //save method run asynchronously
+
 // start listening
 app.listen(5000, "localhost", () => {
   console.log("server is started successfully");
